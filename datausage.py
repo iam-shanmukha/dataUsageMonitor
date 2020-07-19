cab05beaaafa4edcab2c53b804a901388bb077d9
iam-shanmukha <vishnushanmukha@gmail.com>
2020-07-19 11:45:01 +0530

    added GUI

--- datausage.py
+++ datausage.py
@@ -8,7 +8,7 @@ def dataMonitor():
 	os.system("cat /proc/net/dev | grep wlo1  >> wlo1Log")
 	strings =os.popen("cat /proc/net/dev | grep wlo1").read()
 	strings = strings.split(" ")[3]
-	mb =int(strings)/(1024*1024)
+	mb =int(strings)/(1024*1024) #converting Bytes into MegaBytes
 	print("Data used is {:.2f} MB".format(mb))
 	label = Label(root, text= "You have consumed {:.2f} MB since restart".format(mb))
 	label.pack()
