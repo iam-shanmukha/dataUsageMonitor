6417e6c83b76dffc1782181f1dbbbd0795a76624
iam-shanmukha <vishnushanmukha@gmail.com>
2020-07-19 11:31:54 +0530

    added GUI

--- datausage.py
+++ datausage.py
@@ -1,6 +1,16 @@
+#!/usr/bin/python
+from tkinter import *
 import os
-os.system("cat /proc/net/dev | grep wlo1  >> wlo1Log")
-strings =os.popen("cat /proc/net/dev | grep wlo1").read()
-strings = strings.split(" ")[3]
-mb =int(strings)/(1024*1024)
-print("Data used is {:.2f} MB".format(mb))
+root=Tk()
+root.geometry("300x100")
+
+def dataMonitor():
+	os.system("cat /proc/net/dev | grep wlo1  >> wlo1Log")
+	strings =os.popen("cat /proc/net/dev | grep wlo1").read()
+	strings = strings.split(" ")[3]
+	mb =int(strings)/(1024*1024)
+	print("Data used is {:.2f} MB".format(mb))
+	label = Label(root, text= "You have consumed {:.2f} MB since restart".format(mb))
+	label.pack()
+dataMonitor()
+root.mainloop()
